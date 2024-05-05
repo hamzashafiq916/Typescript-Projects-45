38509fbecf4e3d82c43fa48269067f2276ced987
hamzashafiq916 <hamzashafiq916@gmail.com>
2024-05-06 00:29:24 +0500

    change

--- 44.sandwiches/44.sandwiches.ts
+++ 44.sandwiches/44.sandwiches.ts
@@ -2,8 +2,6 @@
 // The function should have one parameter that collects as many items as the function call provides,
 // and it should print a summary of the sandwich that is being ordered.
 
-import { log } from "console";
-
 // Call the function three times, using a different number of arguments each time.
 function make_sandwich(...items: string[]) {
   console.log("Making a sandwich with the following items:\n");
