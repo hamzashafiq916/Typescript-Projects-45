23c5ff497915b5d74ee91a0c0052bb06f4d39f71
hamzashafiq916 <hamzashafiq916@gmail.com>
2024-05-06 16:38:46 +0500

    change

--- 45.cars/45.cars.js
+++ 45.cars/45.cars.js
@@ -1,22 +1,31 @@
 "use strict";
-// Cars: Write a function that stores information about a car in an Object.
-// The function should always receive a manufacturer and a model name.
-// It should then accept an arbitrary number of keyword arguments.
-// Call the function with the required information and two other name-value pairs,
-// such as a color or an optional feature.
-// Print the Object that’s returned to make sure all the information was stored correctly.
-function make_car(manufacturer, model, ...options) {
-    const car = {
+// // Cars: Write a function that stores information about a car in an Object.
+// // The function should always receive a manufacturer and a model name.
+// // It should then accept an arbitrary number of keyword arguments.
+// // Call the function with the required information and two other name-value pairs,
+// // such as a color or an optional feature.
+// // Print the Object that’s returned to make sure all the information was stored correctly.
+// function make_car(manufacturer: string, model: string, ...options: { [key: string]: any }[]) {
+//   const car: { manufacturer: string, model: string, options: { [key: string]: any } } = {
+//     manufacturer: manufacturer,
+//     model: model,
+//     options: {}
+//   };
+// Function to store information about a car
+function storeCarInfo(manufacturer, model, ...details) {
+    const carInfo = {
         manufacturer: manufacturer,
-        model: model,
-        options: {}
+        model: model
     };
-    for (const option of options) {
-        for (const key in option) {
-            car.options[key] = option[key];
-        }
-    }
-    return car;
+    // Add optional details to the car object
+    details.forEach(detail => {
+        Object.keys(detail).forEach(key => {
+            carInfo[key] = detail[key];
+        });
+    });
+    return carInfo;
 }
-const car = make_car('Toyota', 'Corolla', { color: 'blue' }, { sunroof: true });
-console.log(car);
+// Call the function with required and optional information
+const myCar = storeCarInfo('Toyota', 'Camry', { color: 'Silver' }, { year: 2022 }, { features: ['Navigation', 'Leather Seats'] });
+// Print the object returned by the function
+console.log(myCar);
--- 45.cars/45.cars.ts
+++ 45.cars/45.cars.ts
@@ -1,25 +1,46 @@
-// Cars: Write a function that stores information about a car in an Object.
-// The function should always receive a manufacturer and a model name.
-// It should then accept an arbitrary number of keyword arguments.
-// Call the function with the required information and two other name-value pairs,
-// such as a color or an optional feature.
-// Print the Object that’s returned to make sure all the information was stored correctly.
-function make_car(manufacturer: string, model: string, ...options: { [key: string]: any }[]): { manufacturer: string, model: string, options: { [key: string]: any } } {
-  const car: { manufacturer: string, model: string, options: { [key: string]: any } } = {
+// // Cars: Write a function that stores information about a car in an Object.
+// // The function should always receive a manufacturer and a model name.
+// // It should then accept an arbitrary number of keyword arguments.
+// // Call the function with the required information and two other name-value pairs,
+// // such as a color or an optional feature.
+// // Print the Object that’s returned to make sure all the information was stored correctly.
+
+// Define a type for the car object
+type Car = {
+  manufacturer: string;
+  model: string;
+  [key: string]: any; // Allow any other optional properties
+};
+
+// const car = make_car('Toyota', 'Corolla', { color: '
+
+// Function to store information about a car
+function storeCarInfo(
+  manufacturer: string,
+  model: string,
+  ...details: { [key: string]: any }[]
+): Car {
+  const carInfo: Car = {
     manufacturer: manufacturer,
     model: model,
-    options: {}
   };
-
-  for (const option of options) {
-    for (const key in option) {
-      car.options[key] = option[key];
-    }
-  }
-
-  return car;
+  // Add optional details to the car object
+  details.forEach((detail) => {
+    Object.keys(detail).forEach((key) => {
+      carInfo[key] = detail[key];
+    });
+  });
+  return carInfo;
 }
 
-const car = make_car('Toyota', 'Corolla', { color: 'blue' }, { sunroof: true });
-console.log(car);
+// Call the function with required and optional information
+const myCar = storeCarInfo(
+  "Toyota",
+  "Camry",
+  { color: "Silver" },
+  { year: 2022 },
+  { features: ["Navigation", "Leather Seats"] }
+);
 
+// Print the object returned by the function
+console.log(myCar);
